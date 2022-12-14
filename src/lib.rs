9ab3dbab024a90e831bbdbbd66148aa38f85227a
askoufis <askoufis@seek.com.au>
2022-12-14 14:55:47 +1100

    Add `Vec2` utility struct

--- src/lib.rs
+++ src/lib.rs
@@ -17,5 +17,6 @@ pub mod day7;
 pub mod day8;
 pub mod day9;
 pub mod parsers;
+pub mod vec2;
 
 aoc_lib! { year = 2022 }
---
+++ src/vec2.rs
@@ -0,0 +1,42 @@
+use std::ops::{Add, AddAssign, Sub, SubAssign};
+
+pub struct Vec2 {
+    x: isize,
+    y: isize,
+}
+
+impl Add for Vec2 {
+    type Output = Vec2;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl AddAssign for Vec2 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+impl Sub for Vec2 {
+    type Output = Vec2;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl SubAssign for Vec2 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+    }
+}
